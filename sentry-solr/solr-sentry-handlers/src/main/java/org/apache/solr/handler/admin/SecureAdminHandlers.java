c8e69f6ae5eda93623d52ac09805e9e37c3c1df0
Gregory Chanan <gchanan@cloudera.com>
2014-12-08 16:34:41 -0800

    Fixes the SecureAdminHandler issues.
    There still seems to be some issues in the tests around CloudDescriptors.

--- sentry-solr/solr-sentry-handlers/src/main/java/org/apache/solr/handler/admin/SecureAdminHandlers.java
+++ sentry-solr/solr-sentry-handlers/src/main/java/org/apache/solr/handler/admin/SecureAdminHandlers.java
@@ -18,13 +18,18 @@ package org.apache.solr.handler.admin;
 
 import java.io.IOException;
 import java.util.EnumSet;
+import java.util.Map;
+
+import org.apache.solr.common.SolrException;
 import org.apache.solr.core.CoreContainer;
 import org.apache.solr.core.SolrCore;
 import org.apache.sentry.core.model.search.SearchModelAction;
 import org.apache.solr.handler.RequestHandlerBase;
 import org.apache.solr.handler.SecureRequestHandlerUtil;
 import org.apache.solr.request.SolrQueryRequest;
+import org.apache.solr.request.SolrRequestHandler;
 import org.apache.solr.response.SolrQueryResponse;
+import org.apache.solr.util.plugin.SolrCoreAware;
 import org.apache.zookeeper.KeeperException;
 
 /**
@@ -32,18 +37,67 @@ import org.apache.zookeeper.KeeperException;
  */
 public class SecureAdminHandlers extends AdminHandlers {
 
+  protected static class StandardHandler {
+    final String name;
+    final SolrRequestHandler handler;
+    
+    public StandardHandler( String n, SolrRequestHandler h )
+    {
+      this.name = n;
+      this.handler = h;
+    }
+  }
+
+  /**
+   * NOTE: ideally we'd just override the list of admin handlers, but
+   * since AdminHandlers in solr doesn't allow it, let's just copy the
+   * entire function.  This is deprecated in Solr 5.0, so we should do something
+   * different with Solr 5.0 anyway.
+   */
   @Override
-  protected StandardHandler[] getStandardHandlers() {
-    return new StandardHandler[] {
-      new StandardHandler( "luke", new SecureLukeRequestHandler() ),
-      new StandardHandler( "system", new SecureSystemInfoHandler() ),
-      new StandardHandler( "mbeans", new SecureSolrInfoMBeanHandler() ),
-      new StandardHandler( "plugins", new SecurePluginInfoHandler() ),
-      new StandardHandler( "threads", new SecureThreadDumpHandler() ),
-      new StandardHandler( "properties", new SecurePropertiesRequestHandler() ),
-      new StandardHandler( "logging", new SecureLoggingHandler() ),
-      new StandardHandler( "file", new SecureShowFileRequestHandler() )
+  public void inform(SolrCore core) 
+  {
+    String path = null;
+    for( Map.Entry<String, SolrRequestHandler> entry : core.getRequestHandlers().entrySet() ) {
+      if( entry.getValue() == this ) {
+        path = entry.getKey();
+        break;
+      }
+    }
+    if( path == null ) {
+      throw new SolrException( SolrException.ErrorCode.SERVER_ERROR, 
+          "The AdminHandler is not registered with the current core." );
+    }
+    if( !path.startsWith( "/" ) ) {
+      throw new SolrException( SolrException.ErrorCode.SERVER_ERROR, 
+        "The AdminHandler needs to be registered to a path.  Typically this is '/admin'" );
+    }
+    // Remove the parent handler 
+    core.registerRequestHandler(path, null);
+    if( !path.endsWith( "/" ) ) {
+      path += "/";
+    }
+    
+    StandardHandler[] list = new StandardHandler[] {
+       new StandardHandler( "luke", new SecureLukeRequestHandler() ),
+       new StandardHandler( "system", new SecureSystemInfoHandler() ),
+       new StandardHandler( "mbeans", new SecureSolrInfoMBeanHandler() ),
+       new StandardHandler( "plugins", new SecurePluginInfoHandler() ),
+       new StandardHandler( "threads", new SecureThreadDumpHandler() ),
+       new StandardHandler( "properties", new SecurePropertiesRequestHandler() ),
+       new StandardHandler( "logging", new SecureLoggingHandler() ),
+       new StandardHandler( "file", new SecureShowFileRequestHandler() )
     };
+    
+    for( StandardHandler handler : list ) {
+      if( core.getRequestHandler( path+handler.name ) == null ) {
+        handler.handler.init( initArgs );
+        core.registerRequestHandler( path+handler.name, handler.handler );
+        if( handler.handler instanceof SolrCoreAware ) {
+          ((SolrCoreAware)handler.handler).inform(core);
+        }
+      }
+    }
   }
 
   public static class SecureLoggingHandler extends LoggingHandler {
